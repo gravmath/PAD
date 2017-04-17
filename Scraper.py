c6e0be9c46ab651e40558c08d0b4942210511a8f
gravmath <gravmath@yahoo.com>
2017-04-17 17:10:16 -0400

    New PAD Architecture
    New PAD Architecture, including modularized plotting, scraping and
    computing

--- Scraper.py
+++ Scraper.py
@@ -1,11 +1,22 @@
+import matplotlib.dates  as mdates
 import numpy as np
 import os
+import re
 
 ###############################################################################
 ###############################################################################
-#  File Scraping
+#  File Scraping Utility Functions
 ###############################################################################
 ###############################################################################
+def construct_file_selector(obs,mode,descriptor,ver):
+    pattern_str  = '%s_fpi_%s_l2_%s_\d{14}_v%s.cdf' % (obs,mode,descriptor,ver)
+    print 'encoding a search for %s!' % pattern_str
+    return re.compile(pattern_str)
+    
+def file_epoch(filename):
+    basename = os.path.basename(filename)
+    return basename.split('_')[5]
+
 def scrape_files(regex_pattern,targ_dir):
     
     filtered_files   = []
