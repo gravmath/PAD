b2a7083a2e8846583e211d36736e50db30911481
Schiff <cschiff@ndc.nasa.gov>
2018-05-18 18:14:17 -0400

    Vast improvement on vectorizing in Analyzer and the beginning of velcotiy distribution plots

--- src/Analyzer.py
+++ src/Analyzer.py
@@ -1,13 +1,57 @@
 # -*- coding: utf-8 -*-
+import matplotlib.dates        as mdates
 import numpy                   as np
 import scipy                   as sp
+import scipy.interpolate       as interp
 import Munger
 
 num_az         = 32
 num_polar      = 16
 num_ergs       = 32
 num_components = 3
+
+num_pixels     = num_az*num_polar
+num_channels   = num_az*num_polar*num_ergs
 deg            = sp.pi/180.0
+
+############################################################################### 
+def calculate_mec_to_tknot(tknot,mec_munge,frame):
+    """Helper function to interpolate MEC data to a given tknot.
+       The primary use is to decorate the tick labels on a matplotlib plot
+
+       Arguments:
+          tknot:     specific time-knot
+          mec_munge: munge of MEC data - usually ID
+          frame:     'gse' or 'gsm'
+
+       Returns:
+          [xknot, yknot, zknot, mltknot] - 4-D array with the spececraft 
+                                           position and magnetic local time. 
+                                           
+       Example use:  
+           [x,y,z,M] = calc_mec_to_tknot(tknot,mec_munge,frame)
+              
+       Note:  None
+       """    
+
+    if frame == 'gse':
+        mec_interp_x = interp.interp1d(mdates.date2num(mec_munge[0]['epochs']),mec_munge[0]['gse_pos'][:,0])
+        mec_interp_y = interp.interp1d(mdates.date2num(mec_munge[0]['epochs']),mec_munge[0]['gse_pos'][:,1])
+        mec_interp_z = interp.interp1d(mdates.date2num(mec_munge[0]['epochs']),mec_munge[0]['gse_pos'][:,2])
+    if frame == 'gsm':
+        mec_interp_x = interp.interp1d(mdates.date2num(mec_munge[0]['epochs']),mec_munge[0]['gsm_pos'][:,0])
+        mec_interp_y = interp.interp1d(mdates.date2num(mec_munge[0]['epochs']),mec_munge[0]['gsm_pos'][:,1])
+        mec_interp_z = interp.interp1d(mdates.date2num(mec_munge[0]['epochs']),mec_munge[0]['gsm_pos'][:,2])
+    mec_interp_mlt = interp.interp1d(mdates.date2num(mec_munge[0]['epochs']),mec_munge[0]['mlt'][:])
+        
+    Re = 6378.14
+    
+    xknot   = float(mec_interp_x(tknot)/Re)
+    yknot   = float(mec_interp_y(tknot)/Re)
+    zknot   = float(mec_interp_z(tknot)/Re)
+    mltknot = float(mec_interp_mlt(tknot))
+    
+    return [xknot,yknot,zknot,mltknot]
 
 ############################################################################### 
 def make_record_varying(munge,key):
@@ -50,6 +94,44 @@ def make_record_varying(munge,key):
         #resize the NRV data to fit the size and restore 
         #it to the munge
         munge[N][key] = np.tile(temp,(num_tknots,1))        
+        
+###############################################################################         
+def calculate_index(vals,min_val,max_val,num_points):
+    """Analysis function to calculate the indices that map a 1D array of values
+       to a specfic bin.
+
+       Arguments:
+          vals:       1D array of raw values to be mapped to a bin
+          min_val:    minimum considered value in the bin
+          max_val:    maximum considered value in the bin
+          num_points: number of points in the bin
+
+       Returns:
+           index:     1D array of indices stating were the data belongs in 
+                      the bin
+           flag:      1D array of 0's or 1's indicating where the data belongs
+                      in the bin (1) or falls outside (0)
+       
+       Example use:  
+           index, flag = calculate_index(vals,min_val,max_val,num_points)
+              
+       Note:  Assumes a 1D array
+       """    
+    #determine the delta
+    delta  = (max_val - min_val)/(num_points - 1)
+    offset = (num_points - 1)/2
+    
+    index = np.around(vals/delta).astype(int) + offset
+    flag  = np.ones(len(vals))
+    
+    subzero_indices          = np.where(index < 0)
+    over_9000_indices        = np.where(index > num_points - 1)
+    index[subzero_indices]   = 0
+    flag[subzero_indices]    = 0.0
+    index[over_9000_indices] = num_points - 1
+    flag[over_9000_indices]  = 0.0
+        
+    return index, flag        
 
 ############################################################################### 
 def calculate_incoming_particle_directions(sdist_munge):
@@ -85,6 +167,92 @@ def calculate_incoming_particle_directions(sdist_munge):
                                   *sp.sin(sdist_munge[N]['phis'][:,i]*deg)
                 v_dirs[:,i,j,2] = -sp.cos(sdist_munge[N]['thetas'][:,j]*deg)
         sdist_munge[N]['v_dirs'] = v_dirs
+
+###############################################################################         
+def calculate_incoming_particle_velocities(sdist_munge,species):
+    """Analysis function to calculate the particle velocities for each pixel 
+       at each time step and add those data (time, num_channels,vector) to
+       the munge.
+
+       Arguments:
+          sdist_munge: a munge of either electron or ion distribution
+          species:     string of either 'electrons' or 'ions'
+
+       Returns:
+           nothing per se - adds to the munge a time-varying list of 
+           particle velocity vectors
+       
+       Example use:  
+           calculate_incoming_particle_velocities(edist_brst_mms1,'electrons')
+              
+       Note:  works fine with either species and in brst or fast mode
+       """
+    #determine the number of strides
+    num_stride = len(sdist_munge)
+
+    #set physical constants, including the mass of the species
+    c    = 299792.458000 #km/s
+    if species == 'electrons':
+        ms = 0.5109989461e6/c**2 #ev/c^2
+    if species == 'ions':
+        ms = 938.2720813e6/c**2 #ev/c^2
+
+    for N in range(num_stride):
+        #determine the number of tknots
+        num_tknots = len(sdist_munge[N]['epochs'])
+        
+        #tile and reshape the incoming velocity directions
+        v_dirs     = np.tile(sdist_munge[N]['v_dirs'],num_ergs)
+        v_dirs     = v_dirs.reshape(num_tknots,num_channels,num_components)
+        
+        #tile and reshape the energies
+        ergs       = np.tile(sdist_munge[N]['ergs'],num_pixels)
+        ergs       = ergs.reshape(num_tknots,num_channels)
+        
+        #compute the speeds and velocities
+        speeds      = np.sqrt(2.0*ergs/ms)
+        vels        = np.zeros((num_tknots,num_channels,num_components))
+        vels[:,:,0] = speeds*v_dirs[:,:,0]
+        vels[:,:,1] = speeds*v_dirs[:,:,1]
+        vels[:,:,2] = speeds*v_dirs[:,:,2]        
+        
+        #add to the munge
+        sdist_munge[N]['vels']       = vels
+        
+############################################################################### 
+def calculate_B_local_transformation(sfgm_munge,smoms_munge):
+    """Analysis function to calculate the transformation matrices from GSE to
+       local magnetic coordinates (para, perp1, perp2) at each time step and 
+       add those data to the fgm_munge.
+
+       Arguments:
+          sfgm_munge:  fgm_munge adapted to the smoms_munge
+          smoms_munge: a munge of either electron or ion moments
+
+       Returns:
+           nothing per se - adds to the fgm_munge a time-varying list of 
+           transformation matrices
+       
+       Example use:  
+           calculate_B_local_transformation(efgm_munge,emoms_munge)
+              
+       Note:  works fine with either species and in brst or fast mode
+       """
+    #determine the number of strides
+    num_strides = len(smoms_munge)
+    
+   
+    for N in range(num_strides):
+        #determine the number of tknots in the current time series
+        num_tknots = len(sfgm_munge[N]['epochs'])
+        Bgse       = sfgm_munge[N]['Bgse'][:,0:3]
+        U          = smoms_munge[N]['bulk_vs']
+        para       = Bgse/np.linalg.norm(Bgse,axis=1).reshape((num_tknots,1))
+        perp2      = np.cross(Bgse,U)
+        perp2      = perp2/np.linalg.norm(perp2,axis=1).reshape((num_tknots,1))
+        perp1      = np.cross(perp2,para)
+        T          = np.hstack((para,perp1,perp2)).reshape(num_tknots,3,3)      
+        sfgm_munge[N]['T_localB'] = T        
 
 ###############################################################################         
 def calculate_pitch_angles(fgm_munge,sdist_munge):
@@ -126,6 +294,40 @@ def calculate_pitch_angles(fgm_munge,sdist_munge):
         dot_prods[mask,:,:] = np.nan
         sdist_munge[N]['pitch_angs'] = np.arccos(dot_prods)*180.0/np.pi
 
+###############################################################################            
+def calculate_energy_PAD_at_time_and_energy(sdist_munge,timelabel,erg_index,num_angs):
+    """Analysis function to calculate the pitch angle distribution
+    
+
+       Arguments:
+          sdist_munge: a munge of either electron or ion distribution
+          timelabel:   integer specifying the array index in the 'epochs' array
+                       for the time in question
+          erg_index:   integer specifying the array index in the 'ergs' array
+                       for the energy in question
+          num_angs:    integer specifying the number of angular bins for the PAD
+
+       Returns:
+          ang_bins:    angular bins for the PAD
+          PAD:         the desired pitch angle distribution           
+          
+       Example use:  
+           ang_bins, PAD = calculate_energy_PAD_at_time_and_energy(sdist_munge,timelabel,erg_index,num_angs)
+              
+       Note:  None
+       """ 
+    sliced_jn = np.ndarray.flatten(sdist_munge[0]['jn'][timelabel,:,:,erg_index])
+    sliced_pa = np.ndarray.flatten(sdist_munge[0]['pitch_angs'][timelabel,:,:])
+
+    PAD      = np.zeros(num_angs-1)
+    angs     = np.linspace(0,180,num_angs)
+    ang_bins = np.array([(angs[j]+angs[j-1])/2.0 for j in range(1,num_angs)])
+    for N in range(1,num_angs):
+        valid_pts  = np.logical_and(sliced_pa>=angs[N-1],sliced_pa<angs[N])
+        PAD[N-1]   = np.average(sliced_jn[valid_pts])
+    return ang_bins, PAD
+        
+        
 ###############################################################################         
 def calculate_differential_number_flux(sdist_munge,species):
     """Analysis function to calculate the differential number flux of a 
@@ -191,6 +393,66 @@ def calculate_omni_number_flux(sdist_munge):
     for N in range(num_strides):
         omni_jn = np.average(np.average(sdist_munge[N]['jn'][:,:,:,:],axis=1),axis=1)/(4.0*np.pi)
         sdist_munge[N]['omni_jn'] = omni_jn
+
+###############################################################################          
+def calculate_scalar_pressure(smoms_munge):
+    """Analysis function to calculate the scalar pressure of a particle moments
+       munge.
+
+       Arguments:
+          smoms_munge: a munge of either electron or ion moments
+
+       Returns:
+           nothing per se - adds to the sdist_munge a field 'scalar_p' for 
+           differential number flux
+       
+       Example use:  
+           calculate_scalar_pressure(smoms_munge)
+              
+       Note:  None
+       """    
+    #determine the number of strides
+    num_strides = len(smoms_munge)
+    
+    for N in range(num_strides):
+        scalar_p    = np.zeros(len(smoms_munge[N]['epochs']))
+        scalar_p[:] = (smoms_munge[N]['pres_s'][:,0,0]+smoms_munge[N]['pres_s'][:,1,1]+smoms_munge[N]['pres_s'][:,2,2])/3.0
+        
+        smoms_munge[N]['scalar_p'] = scalar_p
+
+###############################################################################          
+def calculate_plasma_beta(fgm_munge,smoms_munge):
+    """Analysis function to calculate the plasma beta for a particle moments
+       munge.
+
+       Arguments:
+          fgm_munge:   a munge for fgm (unadapted)
+          smoms_munge: a munge of either electron or ion moments
+
+       Returns:
+           nothing per se - adds to the sdist_munge a field 'beta' for 
+           differential number flux
+       
+       Example use:  
+           calculate_plasma_beta(fgm_munge,emoms_munge)
+              
+       Note:  None
+       """ 
+    #determine the number of strides
+    num_strides = len(smoms_munge)
+    
+    #make a compatible fgm_munge
+    sfgm_munge = Munger.adapt_munge_to_munge(fgm_munge,smoms_munge)
+    
+    #enter some physical constants
+    mu_0 = 4*np.pi*1.0e-7
+    
+    #calculate plasma beta
+    for N in range(num_strides):
+        p_mag = (sfgm_munge[N]['Bbcs'][:,3]*1.0e-9)**2/(2*mu_0)
+        beta  = smoms_munge[N]['scalar_p']*1.0e-9/p_mag
+        
+        smoms_munge[N]['beta'] = beta      
 
 ###############################################################################
 #
